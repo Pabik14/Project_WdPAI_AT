5fef83391fe28c4e0adb68083aace3441e1a638e
Oskar Lechowicz <oskar.lechowicz7@gmail.com>
2024-10-15 03:52:29 +0200

    adding database connection

---
+++ Database.php
@@ -0,0 +1,31 @@
+<?php
+
+class Database {
+    private $username = 'docker';
+    private $password = 'docker';
+    private $host = 'db';
+    private $database = 'db';
+    private $port = 5432;
+
+
+
+
+    public function connect()
+    {
+        try {
+            $conn = new PDO(
+                "pgsql:host=$this->host;
+                port=$this->port;
+                dbname=$this->database", $this->username, $this->password,
+                ["sslmode"  => "prefer"]
+            );
+            
+            // set the PDO error mode to exception
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $conn;
+        }
+        catch(PDOException $e) {
+            die("Connection failed: " . $e->getMessage());
+        }
+    }  
+}
