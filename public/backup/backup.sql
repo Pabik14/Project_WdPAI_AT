dfd4b8f768db1323dc6403ab7489dacfcfdc6108
Oskar Lechowicz <oskar.lechowicz7@gmail.com>
2024-10-15 04:50:03 +0200

    admin insert

--- public/backup/backup.sql
+++ public/backup/backup.sql
@@ -5,20 +5,23 @@ CREATE TABLE users (
     password VARCHAR(255) NOT NULL
 );
 CREATE TABLE anime_list (
-    id SERIAL PRIMARY KEY, -- Unikalne ID dla każdego dodanego anime
-    user_id INT NOT NULL, -- ID użytkownika dodającego anime, połączenie z drugą tabelą użytkowników
-    anime_name VARCHAR(255) NOT NULL, -- Nazwa anime
+    id SERIAL PRIMARY KEY, 
+    user_id INT NOT NULL, 
+    anime_name VARCHAR(255) NOT NULL, 
     category VARCHAR(50) NOT NULL CHECK (category IN (
         'Akcja', 'Przygodowe', 'Horror', 'Fantasy', 'Sci-Fi', 'Komedia', 
         'Dramat', 'Romans', 'Shounen', 'Shoujo', 'Mecha', 
         'Slice of Life', 'Mystery', 'Thriller', 'Sportowe'
-    )), -- Kategoria anime, ograniczona do wybranych wartości
+    )), 
     type VARCHAR(20) NOT NULL CHECK (type IN (
         'Tv', 'Movie', 'OVA', 'Special'
-    )), -- Typ anime, ograniczony do wybranych wartości
+    )), 
     status VARCHAR(20) NOT NULL CHECK (status IN (
         'watching', 'watched', 'planned', 'on hold', 'abandoned'
-    )), -- Status oglądania anime
-    episodes_count INT CHECK (episodes_count >= 0), -- Liczba odcinków, nie może być ujemna
-    FOREIGN KEY (user_id) REFERENCES users(id) -- Połączenie z tabelą users
+    )), 
+    episodes_count INT CHECK (episodes_count >= 0), 
+    FOREIGN KEY (user_id) REFERENCES users(id) 
 );
+
+INSERT INTO users (name, email, password) 
+VALUES ('Admin', 'admin@admin.pl', '123');
